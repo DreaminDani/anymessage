8982ee472afbd9ba58687c893cfc485f33b806c8
Daniel Sandoval <daniel@desandoval.net>
2019-02-16 12:20:16 -0500

    added missing copyright to files

--- ui/src/components/setup/index.js
+++ ui/src/components/setup/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) AnyMessage.io. All rights reserved. http://www.anymessage.io
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.md file.
+ */
+
 export { default as SetupTeamURL } from './SetupTeamURL';
 export { default as SetupIntegrations } from './SetupIntegrations';
 export { default as SetupPayment } from './SetupPayment';
